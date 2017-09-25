353facfa07cc37b3ef7115d1c6915a55e0f7f2b6
lilpea <ekket@protonmail.ch>
2017-09-25 18:12:22 +0300

    remove one m

--- aaa.py
+++
@@ -1,6 +0,0 @@
-import random
-
-if input("Rock, paper, or scissors? ").lower() in ["rock", "paper", "scissors"]:
-    print(random.choice(["You lose!", "Tie!", "You win!"]))
-else:
-    print("You dunce!")
--- aaaaa.py
+++
@@ -1,6 +0,0 @@
-import random
-
-while input("Rock, paper, or scissors? ").lower() in ["rock", "paper", "scissors"]:
-    print(random.choice(["You lose!", "Tie!", "You win!"]))
-else:
-    print("You dunce!")
